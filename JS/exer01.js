07263d14f0790e851e557e35038300ee563edd6f
rosamariamello@gmail.com <125834014+rosamariamello@users.noreply.github.com>
2024-06-15 17:11:11 -0300

    atualização ativ JS

---
+++ JS/exer01.js
@@ -0,0 +1,13 @@
+// Variável Nome Crie uma variável nome e atribua a ela seu nome. Em seguida, imprima no console e em um elemento HTML.
+var nome
+nome = "Rosamaria"
+console.log("nome")
+
+
+
+
+//Manipule a tag HTML utilizando JavaScript para alterar seu conteúdo para "Olá, Mundo!".
+
+
+
+
---
+++ JS/exer02.js
@@ -0,0 +1,7 @@
+
+//Utilize as palavraschave var, let e const para declarar três variáveis diferentes e imprima seus valores no console.
+var var1 = "1"
+let var2 = "2" 
+const var3 = "3"
+
+console.log(var1, var2, var3,)
